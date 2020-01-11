35c17625017fb9accec73a75328796e4e19c1a4b
Matthew Lucas <mlucas24@icloud.com>
2020-01-11 12:25:54 -0600

    created basic file of user profile

---
+++ client/dist/userProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import store from './store'
+
+const UserProfile = ({ }) => {
+    return (
+        <div>I am user profile</div>
+    )
+}
+export default UserProfile;
