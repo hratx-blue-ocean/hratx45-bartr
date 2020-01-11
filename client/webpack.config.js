2d2c39c73718c5184cb8198427fc8e441fe36800
Gabe-Lee <gla.ander@gmail.com>
2020-01-11 16:26:09 -0600

    Add .babelrc settings directly in webpack
    Remove .babelrc

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,70 +4,72 @@ const DIST_FOLDER = path.resolve(__dirname, 'dist');
 const ENTRY_FILE = './src/index.js';
 
 const config = {
-    entry: ENTRY_FILE,
-    output: {
-        filename: 'bundle.js',
-        path: DIST_FOLDER,
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: '/node_modules',
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
+  entry: ENTRY_FILE,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_FOLDER
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: '/node_modules',
+        use: ['babel-loader'],
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/transform-runtime'
+          ]
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
 
-            {
-                test: /\.(png|jpg|svg|gif|woff|ttf|woff2|eot)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            limit: 1024,
-                            name: 'assets/[path][name].[ext]',
-                            context: 'src/'
-
-                        }
-
-                    },
-                ],
-            },
-
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
+      {
+        test: /\.(png|jpg|svg|gif|woff|ttf|woff2|eot)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 1024,
+              name: 'assets/[path][name].[ext]',
+              context: 'src/'
             }
+          }
         ]
-    },
+      },
+
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  }
 };
 
 if (process.env.NODE_ENV === 'development') {
-    config['mode'] = 'development';
-    config['watch'] = true;
-    config['devServer'] = {
-        contentBase: DIST_FOLDER,
-        compress: true,
-        port: 9000
-    };
+  config['mode'] = 'development';
+  config['watch'] = true;
+  config['devServer'] = {
+    contentBase: DIST_FOLDER,
+    compress: true,
+    port: 9000
+  };
 } else if (process.env.NODE_ENV === 'production') {
-    config['mode'] = 'production';
+  config['mode'] = 'production';
 }
 
 module.exports = config;
-
-
-
