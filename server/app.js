a16d7bc1c372a553676580b818b5cfcdc08ab6af
David Silva <davidsilva2841+accounts+gitthub@gmail.com>
2020-01-12 16:46:44 -0600

    Adding https

--- server/app.js
+++ server/app.js
@@ -19,6 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+app.use (function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 require("./startup/routes")(app);
 
 
@@ -26,6 +37,8 @@ require("./startup/routes")(app);
 
 // Public routing
 app.use('/', express.static(path.join(__dirname, '/public/')));
+
+
 
 
 // --------------------------------------------------------------------------------------------------
