53668165ac61c44338a13317faadae6125056311
David Silva <davidsilva2841+accounts+gitthub@gmail.com>
2020-01-12 16:48:48 -0600

    Adding https

--- server/app.js
+++ server/app.js
@@ -20,16 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-app.use (function (req, res, next) {
-  if (req.secure) {
-    // request was via https, so do no special handling
-    next();
-  } else {
-    // request was via http, so redirect to https
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
-
 require("./startup/routes")(app);
 
 
