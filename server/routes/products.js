5cae76d00fff85173ea75671097af908f46459cd
arwadley <wadley.alyssa@gmail.com>
2020-01-13 15:11:49 -0600

    routes in progress for products

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/tables/products.js");
 
+router.get("/products/:productId", (req, res) => {
+  const productId = req.params.id;
+  db.getProductById(productId)
+    .then(data => res.status(200).send(data))
+    .catch(error => {
+      res.status(404).send(error);
+    });
+});
 
+router.get("/products/category/:categoryId", (req, res) => {
+  const categoryId = req.params.categoryId;
+  db.getProductsByCategory(categoryId)
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
 
-let sql = `SELECT latitude, longitude, state
-FROM users
-WHERE ST_DWithin(geom, ST_MakePoint(-93.65931,37.90138), 10*1609, false);
-`;
+router.get("/products/user/:userId/distance/:proximity", (req, res) => {
+  const userId = req.params.userId;
+  const distance = req.params.proximity;
+  db.getProductsByProximityByUserId(userId, distance)
+    .then(data => res.status(200).send(data))
+    .catch(error => res.status(404).send(error));
+});
+
+router.get(
+  "/products/longitude/:longitudePoint/latitude/:latitudePoint/distance:proximity",
+  (req, res) => {
+    const longitude = req.params.longitude;
+    const latitude = req.params.longitude;
+    const proximity = req.params.proximity;
+    db.getProductsByProximityByLongLat(longitude, latitude, proximity)
+      .then(data => res.status(200).send(data))
+      .catch(error => res.status(404).send(error));
+  }
+);
