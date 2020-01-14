4dc7a45c994d90e509eb0f46cdcdf3421753c0bd
David Silva <davidsilva2841+accounts+gitthub@gmail.com>
2020-01-14 10:34:13 -0600

    Added testing screen

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import HomeScreen from './screens/HomeScreen.jsx';
 import SignupScreen from './screens/SignupScreen.jsx';
+import TestingScreen from "./screens/testing/TestingScreen";
 
 import FeedPage from '../src/screens/FeedPage.jsx';
 // import FeedPage from "../src/screens/ExampleFeedPage.jsx";
@@ -16,6 +17,7 @@ class App extends Component {
       <Router>
         <Navbar />
         {/* <ItemDetailPage /> */}
+        <TestingScreen/>
         <Switch>
           <Route exact path="/dist/">
             <HomeScreen />
