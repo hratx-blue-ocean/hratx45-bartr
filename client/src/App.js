13039f2fef1183130ecb8c1bd4d4cf4fb8d9c33b
Sam Lawson <slawson355@gmail.com>
2020-01-13 19:39:25 -0600

    fixed feedscreen link

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import SignupScreen from "./screens/SignupScreen.jsx";
-// import FeedPage from "../src/screens/FeedPage.jsx";
-import FeedPage from "../src/screens/ExampleFeedPage.jsx";
+
+import FeedScreen from "../src/screens/FeedScreen.jsx";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Navbar />
-
+        <FeedScreen />
         <Switch>
           <Route exact path="/dist/">
             <HomeScreen />
           </Route>
           <Route exact path="/dist/signup">
             <SignupScreen />
-          </Route>
-          <Route exact path="/dist/feed">
-            <FeedPage />
           </Route>
         </Switch>
       </Router>
