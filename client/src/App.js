7cb297c3f466053fff7f0f9fb5b291518eee0ffb
Sam Lawson <slawson355@gmail.com>
2020-01-14 12:34:38 -0600

    adding messaging screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import Navbar from "./components/navbar";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import SignupScreen from "./screens/SignupScreen.jsx";
-=======
-import Navbar from './components/navbar';
-import HomeScreen from './screens/HomeScreen.jsx';
-import SignupScreen from './screens/SignupScreen.jsx';
 import TestingScreen from "./screens/testing/TestingScreen";
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
 
 import FeedScreen from "../src/screens/FeedScreen.jsx";
-import Messages from "../src/screens/Messages.jsx";
+import MessagesScreen from "./screens/MessagesScreen.jsx";
 // import FeedPage from "../src/screens/ExampleFeedPage.jsx";
 
 import ItemDetailPage from "./screens/ItemDetailPage";
@@ -23,16 +17,10 @@ class App extends Component {
     return (
       <Router>
         <Navbar />
-        <FeedScreen />
+        {/* <FeedScreen /> */}
         {/* <ItemDetailPage /> */}
-<<<<<<< HEAD
-        <Messages />
-        <Route exact path="/dist/feed">
-          <FeedScreen />
-        </Route>
-=======
-        <TestingScreen/>
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
+        <MessagesScreen />
+        {/* <TestingScreen /> */}
         <Switch>
           <Route exact path="/dist/">
             <HomeScreen />
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,15 +1,8 @@
 // https://mdbootstrap.com/docs/react/navigation/navbar/
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, useHistory } from "react-router-dom";
 import "../assets/styles/navbar.scss";
-=======
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, Link, useHistory } from 'react-router-dom';
-import '../assets/styles/navbar.scss';
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
 import {
   MDBNavbar,
   MDBNavbarBrand,
@@ -18,40 +11,25 @@ import {
   MDBNavLink,
   MDBCollapse,
   MDBContainer,
-<<<<<<< HEAD
-  MDBHamburgerToggler
-} from "mdbreact";
-=======
   MDBHamburgerToggler,
   MDBBtn
-} from 'mdbreact';
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
+} from "mdbreact";
 
 const collapseType = "collapse2";
 
 const Navbar = ({ title, location }) => {
   const [isOpen, setOpen] = useState(false);
-<<<<<<< HEAD
-  console.log("re-render");
-=======
   const history = useHistory();
-  console.log('re-render, browser location: ', location);
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
+  console.log("re-render, browser location: ", location);
   // const newMessages = useSelector(store => store.messages).unread;
   return (
     <MDBNavbar className="main-color" dark>
       <MDBContainer>
         <MDBNavbarNav left>
           <MDBNavItem>
-<<<<<<< HEAD
-            <MDBNavLink to={"#!"} className="main-color">
-              {"<=="}
-            </MDBNavLink>
-=======
             <MDBBtn className="main-color" onClick={() => history.goBack()}>
-              {'<=='}
+              {"<=="}
             </MDBBtn>
->>>>>>> 68ddc4278488b8c9366463776bc8b4ebcdac3fa8
           </MDBNavItem>
         </MDBNavbarNav>
         <MDBNavbarBrand className="navbar-title">Paperclips</MDBNavbarBrand>
