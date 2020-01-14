0e17821dc3e9db41675bc859859745e7a41604ee
Collin Snyder <collinjacobsnyder@gmail.com>
2020-01-13 20:21:57 -0600

    Followed instructions to move mdb .scss files into working directory

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
     return (
       <Router>
         <Navbar />
+        <ItemDetailPage />
         <Switch>
           <Route exact path="/dist/">
             <HomeScreen />
