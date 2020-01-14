477acfdbc91967ccbc2e2da08591c38d02979ecd
Collin Snyder <collinjacobsnyder@gmail.com>
2020-01-14 12:27:57 -0600

    Create usable Upload Item form ready to send to server

--- client/src/components/MakeOfferForm.js
+++ client/src/components/MakeOfferForm.js
@@ -9,12 +9,38 @@ import {
   MDBModalFooter
 } from "mdbreact";
 
+import "../assets/styles/makeOfferForm.scss";
+
 export default class MakeOfferForm extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      modalOpen: false
+      modalOpen: false,
+      availableItems: [
+        {
+          id: 1,
+          name: "One paperclip",
+          value: 936,
+          image:
+            "https://a.1stdibscdn.com/tiffany-co-paper-clip-14k-yellow-gold-vintage-fine-jewelry-money-mens-jewelry-for-sale/1121189/j_71011511563797289160/7101151_master.jpg"
+        },
+        {
+          id: 2,
+          name: "Rare antique cute baby doll",
+          value: 3,
+          image:
+            "http://crazysven.com/wp/wp-content/uploads/yapb_cache/weird_antiques_kewpie_lamp.bsn7luzlsw004wkc4cc0sw8co.6ylu316ao144c8c4woosog48w.th.JPG"
+        },
+        {
+          id: 3,
+          name: "Nicholas Cage Christmas ornament",
+          value: 10000,
+          image:
+            "https://i.etsystatic.com/18008672/r/il/5dd61b/2089294108/il_1588xN.2089294108_m951.jpg"
+        }
+      ],
+      selectedItems: []
     };
 
     this.toggle = this.toggle.bind(this);
@@ -23,20 +49,95 @@ export default class MakeOfferForm extends PureComponent {
   toggle() {
     this.setState({ modalOpen: !this.state.modalOpen });
   }
+
+  selectItem(item) {
+    let { selectedItems } = this.state;
+    selectedItems.push(item);
+    this.setState({ selectedItems });
+  }
+
+  deselectItem(itemId) {
+    let { selectedItems } = this.state;
+    selectedItems = selectedItems.filter(item => item.id !== itemId);
+    this.setState({ selectedItems });
+  }
+
   render() {
     return (
-      <div>
+      <div id="makeOfferForm">
         <form className="md-form">
           <MDBContainer>
-            <MDBBtn color="default" onClick={this.toggle}>Select Item To Offer</MDBBtn>
+            <MDBBtn color="default" onClick={this.toggle}>
+              Select Item(s) To Offer
+            </MDBBtn>
             <MDBModal isOpen={this.state.modalOpen} toggle={this.toggle}>
               <MDBModalHeader>Your Items</MDBModalHeader>
-              <MDBModalBody>Item List</MDBModalBody>
+              <MDBModalBody>
+                <ul>
+                  <li className="oneItem" id={1}>
+                    <div className="itemSelector">
+                      <MDBInput type="checkbox" />
+                    </div>
+                    <div className="itemContainer">
+                      <div className="itemImage">
+                        <img
+                          width="100px"
+                          src="https://a.1stdibscdn.com/tiffany-co-paper-clip-14k-yellow-gold-vintage-fine-jewelry-money-mens-jewelry-for-sale/1121189/j_71011511563797289160/7101151_master.jpg"
+                        />
+                      </div>
+                      <div className="itemInfo">
+                        <h3>One paperclip</h3>
+                        <p>Value: <span>936</span></p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="oneItem" id={2}>
+                    <div className="itemSelector">
+                      <MDBInput type="checkbox" />
+                    </div>
+                    <div className="itemContainer">
+                      <div className="itemImage">
+                        <img
+                          width="100px"
+                          src="http://crazysven.com/wp/wp-content/uploads/yapb_cache/weird_antiques_kewpie_lamp.bsn7luzlsw004wkc4cc0sw8co.6ylu316ao144c8c4woosog48w.th.JPG"
+                        />
+                      </div>
+                      <div className="itemInfo">
+                        <h3>Rare antique cute baby doll</h3>
+                        <p>Value: <span>3</span></p>
+                      </div>
+                    </div>
+                  </li>
+                  <li className="oneItem" id={3}>
+                    <div className="itemSelector">
+                      <MDBInput type="checkbox" />
+                    </div>
+                    <div className="itemContainer">
+                      <div className="itemImage">
+                        <img
+                          width="100px"
+                          src="https://i.etsystatic.com/18008672/r/il/5dd61b/2089294108/il_1588xN.2089294108_m951.jpg"
+                        />
+                      </div>
+                      <div className="itemInfo">
+                        <h3>Nicholas Cage Christmas ornament</h3>
+                        <p>Value: <span>10000</span></p>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </MDBModalBody>
               <MDBModalFooter>
-                <MDBBtn color="warning" onClick={this.toggle}>Add to Offer</MDBBtn>
+                <MDBBtn color="warning" onClick={this.toggle}>
+                  Add to Offer
+                </MDBBtn>
               </MDBModalFooter>
             </MDBModal>
-            <MDBInput type="textarea" label="Send a message with your offer?" outline/>
+            <MDBInput
+              type="textarea"
+              label="Send a message with your offer?"
+              outline
+            />
             <MDBBtn color="danger">Send Your Offer!</MDBBtn>
           </MDBContainer>
         </form>
--- client/src/components/UploadItemForm.js
+++ client/src/components/UploadItemForm.js
@@ -9,17 +9,115 @@ import {
 } from "mdbreact";
 
 export default class UploadItemForm extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      value: "",
+      description: "",
+      desiredTrade: "",
+      timeConstraints: "",
+      fileList: []
+    };
+
+    this.handleNameInput = this.handleNameInput.bind(this);
+    this.handleValueInput = this.handleValueInput.bind(this);
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleDesiredTradeInput = this.handleDesiredTradeInput.bind(this);
+    this.handleTimeConstraintInput = this.handleTimeConstraintInput.bind(this);
+    this.handleFileInput = this.handleFileInput.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  handleNameInput(e) {
+    e.persist();
+    this.setState({ name: e.target.value });
+  }
+
+  handleValueInput(e) {
+    e.persist();
+    this.setState({ value: e.target.value });
+  }
+
+  handleDescriptionInput(e) {
+    e.persist();
+    this.setState({ description: e.target.value });
+  }
+
+  handleDesiredTradeInput(e) {
+    e.persist();
+    this.setState({ desiredTrade: e.target.value });
+  }
+
+  handleTimeConstraintInput(e) {
+    e.persist();
+    this.setState({ timeConstraints: e.target.value });
+  }
+
+  handleFileInput(e) {
+    this.setState({ fileList: [...e] });
+  }
+
+  submit() {
+    let data = new FormData();
+
+    data.append("name", this.state.name);
+    data.append("value", this.state.value);
+    data.append("description", this.state.description);
+    data.append("desiredTrade", this.state.desiredTrade);
+    data.append("timeConstraints", this.state.timeConstraints);
+    this.state.fileList.forEach(file => data.append("file", file, file.name));
+  }
+
   render() {
     return (
       <div>
         <form className="md-form">
-          <MDBInput label="Item Name*" outline size="lg"/>
-          <MDBInput label="Item Value*" outline size="lg"/>
-          <MDBInput type="textarea" label="Description" outline size="lg"/>
-          <MDBInput label="Desired Trade(s)" outline size="lg"/>
-          <MDBInput label="Time Constraints" outline size="lg"/>
-          <MDBFileInput btnTitle="Choose File(s)" textFieldTitle="Upload Images" btnColor="danger" multiple />
-          <MDBBtn>Upload Item</MDBBtn>
+          <MDBInput
+            label="Item Name*"
+            onChange={this.handleNameInput}
+            value={this.state.name}
+            outline
+            size="lg"
+          />
+          <MDBInput
+            label="Item Value*"
+            onChange={this.handleValueInput}
+            value={this.state.value}
+            outline
+            size="lg"
+          />
+          <MDBInput
+            type="textarea"
+            label="Description"
+            onChange={this.handleDescriptionInput}
+            value={this.state.description}
+            outline
+            size="lg"
+          />
+          <MDBInput
+            label="Desired Trade(s)"
+            onChange={this.handleDesiredTradeInput}
+            value={this.state.desiredTrade}
+            outline
+            size="lg"
+          />
+          <MDBInput
+            label="Time Constraints"
+            onChange={this.handleTimeConstraintInput}
+            value={this.state.timeConstraints}
+            outline
+            size="lg"
+          />
+          <MDBFileInput
+            getValue={this.handleFileInput}
+            btnTitle="Choose File(s)"
+            textFieldTitle="Upload Images"
+            btnColor="danger"
+            multiple
+          />
+          <MDBBtn onClick={this.submit}>Upload Item</MDBBtn>
         </form>
       </div>
     );
