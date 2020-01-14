061616acc827b9e32c94e05d0170575486a8d786
Sam Lawson <slawson355@gmail.com>
2020-01-14 08:14:58 -0600

    created messages skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import Navbar from "./components/navbar";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import SignupScreen from "./screens/SignupScreen.jsx";
 
-import FeedPage from "../src/screens/FeedPage.jsx";
+import FeedScreen from "../src/screens/FeedScreen.jsx";
+import Messages from "../src/screens/Messages.jsx";
 // import FeedPage from "../src/screens/ExampleFeedPage.jsx";
 
 import ItemDetailPage from "./screens/ItemDetailPage";
@@ -16,6 +17,10 @@ class App extends Component {
       <Router>
         <Navbar />
         {/* <ItemDetailPage /> */}
+        <Messages />
+        <Route exact path="/dist/feed">
+          <FeedScreen />
+        </Route>
         <Switch>
           <Route exact path="/dist/">
             <HomeScreen />
---
+++ client/src/screens/Messages.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBDropdown,
+  MDBDropdownItem,
+  MDBDropdownMenu,
+  MDBDropdownToggle,
+  MDBInput
+} from "mdbreact";
+import DD from "../dummy_data/users.js";
+
+const Messages = props => {
+  return (
+    <MDBContainer>
+      <div>{}</div>
+    </MDBContainer>
+  );
+};
+
+export default Messages;
