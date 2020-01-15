3a6754b4525d0f1e598fae776e78a28e955414cb
Sam Lawson <slawson355@gmail.com>
2020-01-15 14:38:14 -0600

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomeScreen from './screens/HomeScreen.jsx';
-import SignupScreen from './screens/SignupScreen.jsx';
-import LoginScreen from './screens/LoginScreen.jsx';
-import UserProfile from './screens/userProfileScreen.jsx';
-import FeedScreen from '../src/screens/FeedScreen.jsx';
-import ItemDetailScreen from './screens/ItemDetailScreen';
-import UploadItemScreen from './screens/UploadItemScreen';
-import MakeOfferScreen from './screens/MakeOfferScreen';
-import MessagesScreen from './screens/MessagesScreen';
-import UpForTradeScreen from './screens/UpForTradeScreen';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import SignupScreen from "./screens/SignupScreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import UserProfile from "./screens/userProfileScreen.jsx";
+import FeedScreen from "../src/screens/FeedScreen.jsx";
+import ItemDetailScreen from "./screens/ItemDetailScreen";
+import UploadItemScreen from "./screens/UploadItemScreen";
+import MakeOfferScreen from "./screens/MakeOfferScreen";
+import MessagesScreen from "./screens/MessagesScreen";
+import UpForTradeScreen from "./screens/UpForTradeScreen";
 class App extends Component {
   render() {
     return (
@@ -34,13 +34,6 @@ class App extends Component {
           <Route exact path="/dist/ItemDetail">
             <ItemDetailScreen />
           </Route>
-<<<<<<< HEAD
-          <Route exact path="/dist/messages">
-            <MessagesScreen />
-          </Route>
-          <Route exact path="/dist/upload">
-            <UploadItemScreen />
-=======
           <Route exact path="/dist/makeOffer">
             <MakeOfferScreen />
           </Route>
@@ -52,7 +45,6 @@ class App extends Component {
           </Route>
           <Route exact path="/dist/trade">
             <UpForTradeScreen />
->>>>>>> 7e734c7fc26dec13ea3c115516a8f51400c1cf06
           </Route>
         </Switch>
       </Router>
