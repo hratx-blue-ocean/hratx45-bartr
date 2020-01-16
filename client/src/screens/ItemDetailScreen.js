3a75f9cd9cf4150b8dd5eacf39678d6baf6985a2
Collin Snyder <collinjacobsnyder@gmail.com>
2020-01-16 09:02:12 -0600

    Item detail commit

--- client/src/screens/ItemDetailScreen.js
+++ client/src/screens/ItemDetailScreen.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { connect } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import {
   fetchProductsByProductId,
@@ -12,40 +12,40 @@ import ItemDetailMakeOfferButton from "../components/ItemDetailMakeOfferButton";
 import "../assets/styles/itemDetail.scss";
 
 const ItemDetailScreen = props => {
+  let { id } = useParams();
+  fetchProductsByProductId(id);
 
-    let {id} = useParams();
-    
-    props.fetchProductsByProductId(id);
-
-    return (
-      <div id="itemDetailScreen">
-        <div className="itemTitleContainer">
-          <h1 className="itemTitle">{props.products.product_name}</h1>
-        </div>
-        {/* <div className="itemOfferCountContainer">
+  return (
+    <div id="itemDetailScreen">
+      <div className="itemTitleContainer">
+        <h1 className="itemTitle">{testProducts.product_name}</h1>
+      </div>
+      {/* <div className="itemOfferCountContainer">
           3 people have bid on this item
         </div> */}
-        <div className="itemDetailImageContainer">
-          <ItemDetailPicture photos={props.products.photos}/>
-        </div>
-        <div className="itemImageCarouselContainer condensed">
-          <ItemDetailCarousel />
-        </div>
-        <div className="itemValueContainer">
-          <h4>
-            Value: <span>{props.products.value}</span>
-          </h4>
-        </div>
-        <div className="makeOfferButtonContainer">
-          <ItemDetailMakeOfferButton />
-        </div>
-        <div className="itemInfoContainer">
-          <ItemDetailItemInfo postedDate={props.products.posted_date} description={props.products.product_description}/>
-        </div>
+      <div className="itemDetailImageContainer">
+        <ItemDetailPicture photos={props.products.photos} />
       </div>
-    );
-
-}
+      <div className="itemImageCarouselContainer condensed">
+        <ItemDetailCarousel />
+      </div>
+      <div className="itemValueContainer">
+        <h4>
+          Value: <span>{props.products.value}</span>
+        </h4>
+      </div>
+      <div className="makeOfferButtonContainer">
+        <ItemDetailMakeOfferButton />
+      </div>
+      <div className="itemInfoContainer">
+        <ItemDetailItemInfo
+          postedDate={props.products.posted_date}
+          description={props.products.product_description}
+        />
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   const { products } = state;
