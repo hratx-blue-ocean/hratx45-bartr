ff042a5e6f032608bb2aeb4d410f8843359a1d8f
Sam Lawson <slawson355@gmail.com>
2020-01-17 14:33:42 -0600

    removed hardcode feedscreen from app

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     return (
       <Router>
         <Navbar />
-        <FeedScreen />
+        {/* <FeedScreen /> */}
         {/* <ItemDetailScreen /> */}
         {/* <!--<TestingScreen />  */}
         {/* <UploadItemScreen />   */}
