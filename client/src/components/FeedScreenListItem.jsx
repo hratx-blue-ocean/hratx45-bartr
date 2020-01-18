8f76c5b692b45751c62c2c4f6755a4f41e398788
Sam Lawson <slawson355@gmail.com>
2020-01-18 10:50:57 -0600

    further feed style

--- client/src/components/FeedScreenListItem.jsx
+++ client/src/components/FeedScreenListItem.jsx
@@ -18,7 +18,7 @@ const FeedScreenListItem = props =>
       style={{ marginTop: "10px", color: "black" }}
     >
       <MDBCardBody>
-        <MDBCard style={{ width: "75vw", textAlign: "left" }}>
+        <MDBCard style={{ width: "75vw", height: "auto", textAlign: "left" }}>
           <Link to={`/ItemDetail/${props.item.product_id}`}>
             <MDBCardImage
               className="img-fluid"
@@ -29,32 +29,60 @@ const FeedScreenListItem = props =>
           </Link>
           <MDBCardTitle
             style={{
+              textAlign: "center",
               marginTop: "5px",
               fontWeight: "bold",
-              marginLeft: "10px"
+              fontSize: "1em"
             }}
           >
             {props.item.product_name}
           </MDBCardTitle>
-          <MDBCardText>
-            <MDBContainer>
+          <MDBCardText style={{ fontWeight: "100" }}>
+            <MDBContainer
+              style={{
+                textAlign: "center",
+                marginTop: "-5px"
+              }}
+            >
               Value: ${props.item.value.toLocaleString()}
             </MDBContainer>
-            <MDBContainer style={{ color: "black" }}>
-              Date Posted: {props.item.posted_date}
-            </MDBContainer>
-            <MDBContainer style={{ color: "black" }}>
+            <MDBContainer
+              style={{ color: "black", textAlign: "right", marginTop: "10%" }}
+            >
               Available? {props.item.up_for_trade === "True" ? "Yes" : "No"}
             </MDBContainer>
           </MDBCardText>
           <MDBContainer
-            style={{ width: "auto", marginRight: "3px", marginBottom: "5px" }}
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between"
+            }}
           >
-            <Link to={`/ItemDetail/${props.item.product_id}`}>
-              <MDBBtn style={{ width: "100%", justifyContent: "center" }}>
-                View Item...
-              </MDBBtn>
-            </Link>
+            <MDBContainer
+              style={{
+                color: "black",
+                padding: "0",
+                width: "auto",
+                fontSize: "x-small"
+              }}
+            >
+              Posted: {props.item.posted_date}
+            </MDBContainer>
+            <MDBContainer
+              style={{
+                width: "auto",
+                marginRight: "3px",
+                marginBottom: "5px",
+                float: "right"
+              }}
+            >
+              <Link to={`/ItemDetail/${props.item.product_id}`}>
+                <MDBBtn style={{ width: "100%", justifyContent: "center" }}>
+                  View Item...
+                </MDBBtn>
+              </Link>
+            </MDBContainer>
           </MDBContainer>
         </MDBCard>
       </MDBCardBody>
