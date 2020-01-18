8f7e2fa98bda166987ba1daf41ea8f04c0cd6d97
Matthew Lucas <mlucas24@icloud.com>
2020-01-18 10:54:49 -0600

    user profile upload

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,4 +1,7 @@
-export default (state = { userName: '', userId: 1, userItems: [] }, action) => {
+export default (
+  state = { userName: '', userId: -1, userItems: [] },
+  action
+) => {
   switch (action.type) {
     case 'FETCH_USER_INFORMATION_BY_USERNAME':
       return action.payload;
