760cc64592fc1598780e2b0f66dfaba1a7331f96
Sam Lawson <slawson355@gmail.com>
2020-01-17 18:34:27 -0600

    i

--- client/src/screens/FeedScreen.jsx
+++ client/src/screens/FeedScreen.jsx
@@ -3,6 +3,7 @@ import Axios from "axios";
 import FeedScreenListItem from "../components/FeedScreenListItem.jsx";
 import FeedScreenDropDown from "../components/FeedScreenDropDown.jsx";
 import { MDBBtn, MDBContainer, MDBInput } from "mdbreact";
+import { store } from "../index.js";
 
 class FeedScreen extends React.Component {
   constructor(props) {
@@ -143,6 +144,7 @@ class FeedScreen extends React.Component {
 
   componentDidMount() {
     this.getProducts();
+    console.log(store.getState());
   }
 
   render() {
