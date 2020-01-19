864864dad34e155351cc35be83e2c53613f7fc41
Gabe-Lee <gla.ander@gmail.com>
2020-01-19 14:40:02 -0600

    Add max width for navbar on desktop

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,26 +1,27 @@
 // Sentry error capturing
-import * as Sentry from '@sentry/browser';
+import * as Sentry from "@sentry/browser";
 Sentry.init({
-  dsn: 'https://7f28de32de2f4076aa054b2e8aaf4686@sentry.io/1878885'
+  dsn: "https://7f28de32de2f4076aa054b2e8aaf4686@sentry.io/1878885"
 });
 
 // http://api-server.escxwv87wi.us-west-2.elasticbeanstalk.com/api/testing/test-postgres/products
-import paperclips from '../apis/paperclips';
-import { GET_UP_FOR_TRADE } from './productsActions';
+import paperclips from "../apis/paperclips";
+import { getActiveUserOffers, getClosedUserOffers } from "./offerActions";
+import { fetchProductsUpForTradeByUserId } from "./productsActions";
 
 // const singupNewUser()
 
 /* Authenticate Username and Password. Returns a boolean. */
 export const authenticateUser = (username, password) => {
   return async (dispatch, getState) => {
-    const response = await paperclips.get('/users/authentication', {
+    const response = await paperclips.get("/users/authentication", {
       params: {
         username: username,
         password: password
       }
     });
     dispatch({
-      type: 'AUTHENTICATE_USER',
+      type: "AUTHENTICATE_USER",
       payload: response.data
     });
   };
@@ -28,24 +29,17 @@ export const authenticateUser = (username, password) => {
 
 export const fetchUserInformationByUsername = username => {
   return async (dispatch, getState) => {
-    const response = await paperclips.get('/users/userInformationUsername', {
+    const response = await paperclips.get("/users/userInformationUsername", {
       params: {
         username: username
       }
     });
-    console.log(response);
     const userId = response.data.rows[0].user_id;
-    const response2 = await paperclips.get('/products/productsUpForTrade', {
-      params: { userId }
-    });
-    let data = response2.data.rows;
-    console.log(response2);
+    getActiveUserOffers(userId)(dispatch, getState);
+    getClosedUserOffers(userId)(dispatch, getState);
+    fetchProductsUpForTradeByUserId(userId)(dispatch, getState);
     dispatch({
-      type: GET_UP_FOR_TRADE,
-      payload: data
-    });
-    dispatch({
-      type: 'FETCH_USER_INFORMATION_BY_USERNAME',
+      type: "FETCH_USER_INFORMATION_BY_USERNAME",
       payload: { userInfo: response.data.rows[0] }
     });
   };
@@ -53,13 +47,13 @@ export const fetchUserInformationByUsername = username => {
 
 export const fetchUserInformationById = userId => {
   return async (dispatch, getState) => {
-    const response = await paperclips.get('/users/userInformationId', {
+    const response = await paperclips.get("/users/userInformationId", {
       params: {
         id: userId
       }
     });
     dispatch({
-      type: 'FETCH_USER_INFORMATION_BY_ID',
+      type: "FETCH_USER_INFORMATION_BY_ID",
       payload: response.data
     });
   };
